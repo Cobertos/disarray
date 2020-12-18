1a4e5964825601eedaf9f56418cd10e03104f15b
cobertos <me+git@cobertos.com>
2020-12-18 04:58:50 -0500

    fix: Loader

--- components/util/Loader.vue
+++ components/util/Loader.vue
@@ -15,20 +15,26 @@ export default {
   vertical-align:middle;
   font-size:0;
   pointer-events: none;
-  width: 25px;
-  height: 25px;
+  box-sizing: content-box;
+  width: 1rem;
+  height: 1rem;
   border-radius: 50%;
-  border: 3px solid rgba(255,255,255,0.3);
+  border: 4px solid rgba(255,255,255,0.3);
   border-left-color: #FFF;
   background: transparent;
-  animation: loading 2.4s cubic-bezier(.21,.51,.48,.51) infinite;
+  animation: loading 3.0s cubic-bezier(.21,.51,.48,.51) infinite;
+
+  &.dark:before {
+    border-color: rgba(0, 0, 0, 0.2);
+    border-left: 4px solid #000;
+  }
 }
 @keyframes loading {
   0% {
     transform: rotateZ(0deg);
   }
   100% {
-    transform: rotatez(360deg);
+    transform: rotateZ(360deg);
   }
 }
 </style>
