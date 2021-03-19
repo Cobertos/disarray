feecb3f4930bf66eb0b3d06673cebcd1eec7bac0
cobertos <me+git@cobertos.com>
2021-03-19 03:24:59 -0400

    Changed name (Disorganized -> Disarray), and fixed the header styles, added subtitle

--- pages/index.vue
+++ pages/index.vue
@@ -9,18 +9,21 @@
         <h2
           class="dc-hero-title"
         >
-          Disorganized
+          Disarray
         </h2>
+        <p class="dc-hero-subtitle">
+          Find Discord communities
+        </p>
       </div>
-      <div>
+      <div class="dc-hero-right">
         <p class="dc-hero-description">
-          Search engine for quality Discord servers and communities.
+          Search engine for quality Discord servers and communities
           <br><br>
-          Disorganized scours the web for public Discord servers and brings them all here. Our algorithm finds more niche communities than you might find on other sites.
+          Disarray scours the web for public Discord servers and brings them all here. Our algorithm finds more niche communities than you might find on other sites.
           <br><br><br>
           Have a question? Join our <a href="https://discord.gg/E5b5y8ue" target="_blank">Discord server</a>.
 
-          <!--TODO: Add a modal with more content: Similar to Google, Disorganized uses a web crawler to find links around the Internet. This means we aren't restricted to admins actively uploading their servers into our database. There are downsides to this approach though. * Can't section by region of world, * Can't filter by active servers-->
+          <!--TODO: Add a modal with more content: Similar to Google, Disarray uses a web crawler to find links around the Internet. This means we aren't restricted to admins actively uploading their servers into our database. There are downsides to this approach though. * Can't section by region of world, * Can't filter by active servers-->
         </p>
       </div>
     </section>
@@ -79,13 +82,13 @@ export default {
   flex-direction: column;
   font-size: 50px;
   text-align: center;
-  height: 40vh;
+  min-height: 40vh;
 
-  margin: 0 10px;
+  margin: 0 10px 50px;
 
   @include desktop {
     flex-direction: row;
-    height: 60vh;
+    min-height: 55vh;
     margin: 0 auto;
     padding: 0 50px;
     justify-content: space-between;
@@ -93,20 +96,44 @@ export default {
   }
 
   > * {
-    flex: 0 0 20%;
+    flex: 0 1 20%;
 
     @include desktop {
-      flex: 0 0 50%;
+      flex: 0 1 50%;
+    }
+  }
+
+  .dc-hero-right {
+    margin-top: 30px;
+
+    @include desktop {
+      margin-top: 0;
+      margin-left: 40px;
     }
   }
 
   .dc-hero-title {
     text-align: center;
     font-family: $do-hyeon;
-    font-size: 100px;
+    font-size: 23vw; // Dynamically grow
     color: #FFF;
     text-shadow: #ccc 0 1px 0, #c9c9c9 0 2px 0, #bbb 0 3px 0, #b9b9b9 0 4px 0, #aaa 0 5px 0,rgba(0,0,0,.1) 0 6px 1px, rgba(0,0,0,.1) 0 0 5px, rgba(0,0,0,.3) 0 1px 3px, rgba(0,0,0,.15) 0 3px 5px, rgba(0,0,0,.2) 0 5px 10px, rgba(0,0,0,.2) 0 10px 10px, rgba(0,0,0,.1) 0 20px 20px;
     transform: rotate(-5deg);
+
+    @include desktop {
+      font-size: 100px;
+    }
+  }
+
+  .dc-hero-subtitle {
+    font-size: 5vw; // Dynamically grow
+    color: #FFF;
+    text-shadow: #ccc 0 1px 0, #c9c9c9 0 2px 0, #bbb 0 3px 0, #b9b9b9 0 4px 0, #aaa 0 5px 0,rgba(0,0,0,.1) 0 6px 1px, rgba(0,0,0,.1) 0 0 5px, rgba(0,0,0,.3) 0 1px 3px, rgba(0,0,0,.15) 0 3px 5px, rgba(0,0,0,.2) 0 5px 10px, rgba(0,0,0,.2) 0 10px 10px, rgba(0,0,0,.1) 0 20px 20px;
+    transform: rotate(-5deg);
+
+    @include desktop {
+      font-size: 30px;
+    }
   }
 
   .dc-hero-description {
